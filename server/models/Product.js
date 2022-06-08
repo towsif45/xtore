32aece7cd2f8d1a9f9cd3bd70e8c718161187b9b
Towsif Hossain <towsifhossain5120@gmail.com>
2022-06-09 03:14:15 +0600

    Add schemas and routes

---
+++ server/models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
