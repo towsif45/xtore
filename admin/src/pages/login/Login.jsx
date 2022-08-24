88aaeeb896a14f53866664d582d569a57953512b
Towsif Hossain <towsifhossain5120@gmail.com>
2022-08-24 19:05:13 +0600

    Admin template added

---
+++ admin/src/pages/login/Login.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault()
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleClick} > Log in </button>
+    </div>
+  );
+};
